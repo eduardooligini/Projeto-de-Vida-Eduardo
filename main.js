115e5c90f5b35db7464e6eeb6d8f06ef5db586aa
eduardooligini <145785071+eduardooligini@users.noreply.github.com>
2025-06-24 08:19:33 -0300

    24

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i=0; i<botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-07-11T00:00:00")
 
-contadores[0].textContent = calculaTempo(tempoObjetivo)
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
